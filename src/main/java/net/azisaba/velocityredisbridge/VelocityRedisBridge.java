21a0e9f90cc5afd417f84479af9909f77002a070
siloneco <siloneco4646@gmail.com>
2022-11-22 17:37:20 +0900

    :bookmark: bump version

--- src/main/java/net/azisaba/velocityredisbridge/VelocityRedisBridge.java
+++ src/main/java/net/azisaba/velocityredisbridge/VelocityRedisBridge.java
@@ -29,7 +29,7 @@ import redis.clients.jedis.JedisPoolConfig;
 @Plugin(
     id = "velocity-redis-bridge",
     name = "VelocityRedisBridge",
-    version = "2.0.3",
+    version = "2.0.4",
     url = "https://github.com/AzisabaNetwork/VelocityRedisBridge",
     description = "Plugin to help parallelization of Velocity",
     authors = {"Azisaba Network"})
